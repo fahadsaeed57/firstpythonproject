de1936b8beef24f9e64ebe4cef64120f6d463a1d
Fahad Saeed <fahadsaeed57@gmail.com>
2017-10-24 11:34:30 +0500

    Class

--- class.py
+++ class.py
@@ -1,4 +1,4 @@
-class numbers:
+class BasicMaths:
     def factorial(self,x):
         fact=1
         for i in range(x,1,-1):
@@ -6,5 +6,5 @@ class numbers:
         print("fact is "+str(fact))
 
 
-num1 = numbers()
+num1 = BasicMaths()
 num1.factorial(5)
